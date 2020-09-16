fedcae58f1b09c36c524a7995c35d3e7fa36f56c
Hagai Wechsler <hagaywec@gmail.com>
2020-09-16 12:17:50 +0300

    first working version

--- sorter.py
+++ sorter.py
@@ -7,40 +7,85 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'eyed3'])
 import eyed3
 
 
-TEMP_PATH = "{rootdir}.sorter_tmp"
-SORTED_PATH = "{rootdir}.sorted"
-
-
 class Sorter(object):
     def __init__(self, rootdir):
         self.rootdir = rootdir
-        self.temp_path = "{root}.sorter_tmp".format(root=self.rootdir)
         self.sorted_path = "{root}.sorted".format(root=self.rootdir)
+        self.track_names = filter(self.is_mp3,
+                                  glob.iglob("{}\\**".format(self.rootdir),
+                                             recursive=True))
+        self.dirtree_format = "{tmp}\\{artist}\\{album}"
+        self.noalbum_dirtree_format = "{tmp}\\{artist}"
+        self.trackfile_path_format = "{dirtree}\\{title}"
 
-    def handle_file(self, path):
-        track = eyed3.load(path)
-        dst_tree = "{}\\{}\\{}".format(self.temp_path,
-                                         track.tag.artist,
-                                         track.tag.album)
-        new_path = "{}\\{}.mp3".format(dst_tree, track.tag.title).replace("/",
-                                                                          "")
-        os.makedirs(dst_tree)
-        # change to os.move when working properly
-        shutil.copy(path, dst_tree)
-        os.rename("{}\\{}".format(dst_tree, os.path.basename(path)), new_path)
-        # TODO: log an indicative message about moving the song file to 
-            # another folder
-
-    def sort(self):
-        os.mkdir(self.temp_path)
-        ifiles = glob.iglob("{}\**".format(self.rootdir), recursive=True)
+    def is_mp3(self, path):
+        return True if os.path.splitext(path)[1] == ".mp3" else False
         # TODO - log an indicative message about discarding the non-mp3 file
             # from the sorter and keeping it where it is.
-        ifiles = filter(
-            lambda p: True if os.path.splitext(p)[1] == ".mp3" else False,
-            ifiles)
-        for file in ifiles:
-            self.handle_file(file)
+
+    def validate_titlename(self, title):
+        """
+        Solves problems such as:
+            - When certain characters collide with python literals
+        """
+        if title:
+            trans = str.maketrans({"/": "-", "*": "", "?": "", '"': "'"})
+            return title.translate(trans)
+        else:
+            return track_name
+
+    def validate_album_artist(self, name):
+        """
+        Solves problems such as:
+            - os.mkdir auto-strips spaces. Then when trying to find the dir 
+              created, no dir is found.
+        """
+        if name:
+            trans = str.maketrans({":": "-", '"': "'"})
+            name = name.translate(trans)
+            name = name.strip()
+            return name
+        else:
+            return name
+
+    def relocate_track(self, track):
+        track_name = os.path.basename(track.path)
+        artist = self.validate_album_artist(track.tag.artist)
+        album = self.validate_album_artist(track.tag.album)
+        title = self.validate_titlename(track.tag.title) 
+        if not album:
+            dirtree = self.noalbum_dirtree_format.format(tmp=self.sorted_path,
+                                                         artist=artist)
+        else:
+            dirtree = self.dirtree_format.format(tmp=self.sorted_path,
+                                                 artist=artist,
+                                                 album=album)
+        new_path = self.trackfile_path_format.format(dirtree=dirtree,
+                                                     title=track_name)
+        renamed = self.trackfile_path_format.format(dirtree=dirtree,
+                                                    title=title) + ".mp3"
+        if os.path.exists(renamed): # song already in destination folder
+            pass
+        else:
+            os.makedirs(dirtree, exist_ok=True)
+            # change to os.move when working properly
+            shutil.copy(track.path, new_path)
+            os.rename(new_path, renamed)
+            # TODO: log an indicative message about moving the song file to
+                # another folder
+
+    def handle_track(self, path):
+        track = eyed3.load(path)
+        if not track.tag or any((not track.tag.artist, not track.tag.title)):
+            return None
+        else:
+            self.relocate_track(track)
+
+    def sort(self):
+        os.mkdir(self.sorted_path)
+        for track in self.track_names:
+            print(track)
+            self.handle_track(track)
 
 
 def main():
